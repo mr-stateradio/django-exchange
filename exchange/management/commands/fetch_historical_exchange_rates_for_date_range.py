9f3fc06dfa85af05a968b9c43e7f93d7cf68ef31
Thomas Kremmel <thomas.kremmel@gmail.com>
2013-04-03 16:04:09 +0200

    add today option to management command

--- exchange/management/commands/fetch_historical_exchange_rates_for_date_range.py
+++ exchange/management/commands/fetch_historical_exchange_rates_for_date_range.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 from optparse import make_option
 from time import mktime, strptime
 
@@ -7,7 +7,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from exchange.utils import import_class, daterange
 from exchange.adapters import BaseAdapter
-from exchange.models import Currency
+from exchange.models import Currency, ExchangeRateHistorical
 
 
 class Command(BaseCommand):
@@ -15,6 +15,7 @@ class Command(BaseCommand):
     :py:class:`exchange.adapters.BaseAdapter` implementation given by
     ``class`` option
     start_date and end_date have to be provided as args
+    if today option is provided start_date and end_date is set to todays date
 
     """
     option_list = BaseCommand.option_list + (
@@ -23,6 +24,9 @@ class Command(BaseCommand):
             default='exchange.adapters.'
                     'openexchangerates.OpenExchangeRatesAdapter'
         ),
+        make_option('--today',
+            action='store_true',
+            help='If this option is provided exchange rates for today will be fetched.'),
         make_option('--startdate', dest='startdate',
                     help='Please provide a start date in the format YYYY-MM-DD'),
         make_option('--enddate', dest='enddate',
@@ -35,23 +39,37 @@ class Command(BaseCommand):
         """
         date_format = "%Y-%m-%d"
         adapter_class_name = options['class']
+        execute_update = True
         # make sure file option is present
-        if options['startdate'] == None :
-            raise CommandError("Option `--startdate=...` must be provided.")
+        if options.get('today', False):
+            self.stdout.write('...fetch todays exchange rates')
+            start_date = date.today()
+            end_date = date.today()
+            #check if all exchange rates for today are already present - if yes - cancel thread
+            curr_count = Currency.objects.all().count()
+            target_number = curr_count * curr_count
+            exchange_rate_count = ExchangeRateHistorical.objects.filter(date=start_date).count()
+            if target_number == exchange_rate_count:
+                #cancel termination - all exchange rates already available
+                execute_update = False
+                self.stdout.write('..todays exchange rates already available')
         else:
-            #check format of input
-            try:
-                start_date = datetime.strptime(options['startdate'], date_format).date()
-            except Exception, e:
-                raise CommandError("Option `--startdate=...` must be provided in the format YYYY-MM-DD.")
-        if options['enddate'] == None :
-            raise CommandError("Option `--enddate=...` must be provided.")
-        else:
-            #check format of input
-            try:
-                end_date = datetime.strptime(options['enddate'], date_format).date()
-            except:
-                raise CommandError("Option `--enddate=...` must be specified in the format YYYY-MM-DD.")
+            if options['startdate'] == None :
+                raise CommandError("Option `--startdate=...` must be provided.")
+            else:
+                #check format of input
+                try:
+                    start_date = datetime.strptime(options['startdate'], date_format).date()
+                except Exception, e:
+                    raise CommandError("Option `--startdate=...` must be provided in the format YYYY-MM-DD.")
+            if options['enddate'] == None :
+                raise CommandError("Option `--enddate=...` must be provided.")
+            else:
+                #check format of input
+                try:
+                    end_date = datetime.strptime(options['enddate'], date_format).date()
+                except:
+                    raise CommandError("Option `--enddate=...` must be specified in the format YYYY-MM-DD.")
 
         try:
             adapter_class = import_class(adapter_class_name)
@@ -67,19 +85,14 @@ class Command(BaseCommand):
             except ValueError:
                 first_name, last_name = arg, None
         
-        #get base currency
-        try:
-            default_currency = getattr(settings, 'DEFAULT_CURRENCY')
-        except Exception, e:
-            raise CommandError("Please define the 'DEFAULT_CURRENCY' setting in setting.py.")
-        
-        currencies = Currency.objects.all()
-        if len(currencies) == 0:
-            raise CommandError("No currencies setup in database. You have to run the update_exchange_rate management command prior to running this command.")
-        
-        #for each day in date range call update_historical_exchange_rates
-        for curr_date in daterange(start_date, end_date, mode=1):
-            self.stdout.write('...fetching rates for date %s ' % (curr_date))
-            for source in currencies:
-                adapter.update_historical_exchange_rates(source, curr_date)
-        self.stdout.write('Successfully fetched exchange rates for date range %s - %s.' % (start_date, end_date))
+        if execute_update == True:
+            currencies = Currency.objects.all()
+            if len(currencies) == 0:
+                raise CommandError("No currencies setup in database. You have to run the update_exchange_rate management command prior to running this command.")
+            
+            #for each day in date range call update_historical_exchange_rates
+            for curr_date in daterange(start_date, end_date, mode=1):
+                self.stdout.write('...fetching rates for date %s ' % (curr_date))
+                for source in currencies:
+                    adapter.update_historical_exchange_rates(source, curr_date)
+            self.stdout.write('Successfully fetched exchange rates for date range %s - %s.' % (start_date, end_date))
